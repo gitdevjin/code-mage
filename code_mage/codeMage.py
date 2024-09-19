c93219e621794ec8fbc05e35ddfc15b320e4e31a
Arina Kolodeznikova <arinak1017@gmail.com>
2024-09-19 17:06:28 -0400

    Modified the --help infor for token flag

--- code_mage/codeMage.py
+++ code_mage/codeMage.py
@@ -18,7 +18,7 @@ def main():
     parser.add_argument('--language', '-l', help='The language to translate the source files into')
     parser.add_argument('--output', '-o', help="Specify the output file name(without extension)")
     parser.add_argument('--version', '-v', action='version', version=f'CodeMage {VERSION}', help="Show program's version number and exit")
-    parser.add_argument('--token-usage', '-t', action='store_true', help='Specify token usage for the prompt and response')
+    parser.add_argument('--token-usage', '-t', action='store_true', help='Get information about token usage for the prompt and response')
     
     args = parser.parse_args()
 
