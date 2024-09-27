16dfd9e80671ba5c5d76ed516eef67b4c05a0118
gitdevjin <dev.hjshin@gmail.com>
2024-09-27 14:11:54 -0400

    overlapping issue in streaming out the output fixed, and some codes refactored

--- code_mage/translator.py
+++ code_mage/translator.py
@@ -11,59 +11,36 @@ def translate(source_file, args, num=""):
 
     supported_lang = ["javascript", "python", "c++", "java"]
 
-    # Get arguments
-    target_lang = args.language
-    output = args.output
-    token_flag = args.token_usage
-    source_lang = "none"
-    
-    # Extracting Original file name and extension
+     # Extracting Original file name and extension
     original_file_name = os.path.splitext(os.path.basename(source_file))[0]
     original_file_ext = os.path.splitext(os.path.basename(source_file))[1]
 
-    # Validify and Set Default traget_lang
-    if target_lang is None:
-        target_lang = "python"
-    elif target_lang not in supported_lang:
+
+    # Decide and Validify Target language into which the source file is translated
+    target_lang = args.language if args.language is not None else "python"
+    if target_lang not in supported_lang:
         sys.exit("the target language is not supported")
 
-    # Default target_lang and extension is python
-    output_file_ext = ".py"
 
-    # Decide output file extension.
-    if target_lang == "javascript":
-        output_file_ext = ".js"
-    elif target_lang == "python":
-        output_file_ext = ".py"
-    elif target_lang == "c++":
-        output_file_ext = ".cpp"
-    elif target_lang == "java":
-        output_file_ext = ".java"
-    
-    # Check source_lang
-    if original_file_ext == ".js":
-        source_lang = "javascript"
-    elif original_file_ext == ".py":
-        source_lang = "python"
-    elif original_file_ext == ".cpp":
-        source_lang = "c++"
-    elif original_file_ext == ".java":
-        source_lang = "java"
-
-    # Validify source_lang
+    # Decide and Validify Input File Language
+    source_lang = get_source_lang(original_file_ext)
     if source_lang == target_lang:
         sys.exit("the target language is the same with the source_file")
     elif source_lang == "none":
         sys.exit("the file is not a supported programming language")
-
     
+
+    # Decide output file name
+    output_file_ext = get_output_ext(target_lang)
     output_file_name = f"translated_{original_file_name}{output_file_ext}"
-    if output:
-        output_file_name = output + str(num) + output_file_ext
+    
+    if args.output:
+        output_file_name = args.output + str(num) + output_file_ext
 
     # Read file
     with open(source_file, 'r') as src:
         code = src.read()
+
 
     # Stream out the result
     if args.stream:
@@ -73,7 +50,9 @@ def translate(source_file, args, num=""):
             if chunk.choices[0].delta.content is not None:
                 print(chunk.choices[0].delta.content, end="")
 
-        if token_flag is not None:
+        sys.stdout.flush()
+
+        if args.token_usage is not None:
             sys.stderr.write("\nStream Flag Doesn't Support Token-Usage Option")
         
         print("\n************ END ************\n");
@@ -86,8 +65,8 @@ def translate(source_file, args, num=""):
         with open(output_file_name, 'w') as f:
             f.write(result)
 
-        # prints token usage information if --token-usage/-t flag is present
-        if token_flag and args.model != "openrouter":
+        # prints token usage info if --token-usage/-t flag is present
+        if args.token_usage and args.model != "openrouter":
             prompt_tokens = completion.usage.prompt_tokens
             completion_tokens = completion.usage.completion_tokens
             total_tokens = completion.usage.total_tokens
@@ -95,10 +74,40 @@ def translate(source_file, args, num=""):
             sys.stderr.write(f"prompt tokens: {prompt_tokens}\n")
             sys.stderr.write(f"completion tokens: {completion_tokens}\n")
             sys.stderr.write(f"total tokens: {total_tokens}\n")
-        elif token_flag and args.model == "openrouter":
+        elif args.token_usage and args.model == "openrouter":
             sys.stderr.write("Sorry, this model doesn't provide token usage details\n")
         
 
+def get_source_lang(extension):
+    src_lang = "none"
+
+    if extension == ".js":
+        src_lang = "javascript"
+    elif extension == ".py":
+        src_lang = "python"
+    elif extension == ".cpp":
+        src_lang = "c++"
+    elif extension == ".java":
+        src_lang = "java"
+
+    return src_lang
+
+
+def get_output_ext(language):
+     # Default target_lang and extension is python
+    output_ext = ".py"
+
+    # Decide output file extension.
+    if language == "javascript":
+        output_ext = ".js"
+    elif language == "python":
+        output_ext = ".py"
+    elif language == "c++":
+        output_ext = ".cpp"
+    elif language == "java":
+        output_ext = ".java"
+
+    return output_ext
 
 
     
